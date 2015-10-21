623ae9d0a403cbaa2bb59b241f7ccfb0ecdf964e
Flavia Fernanda Rocha Moraes <flavia@async.com.br>
2015-10-21 15:34:18 -0200

    New animation for loading buttons
    Change-Id: Iac582adbf643b87b4c6224db7dfbfe993c23898a

---
+++ loading-button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var LoadingButton = React.createClass({
+  render: function() {
+    return <button {...this.props} disabled={this.props.loading}>
+      {this.props.loading ? <i className="fa fa-refresh fa-spin"/>  : this.props.children}
+    </button>
+  },
+});
+
+export default LoadingButton
