7075fde42d824399b9e81c7f66b469145ab5eb93
Eduardo Nishizuka <nishizuka23@gmail.com>
2016-02-05 12:18:44 -0200

    Remove duplicate code by creating 'components/utils/htsql'
    Change-Id: Id43e0e14a7cd38c0dcf9bd5fb0e696c3226077ae

---
+++ utils/htsql.jsx
@@ -0,0 +1,26 @@
+var HTSQL = {
+
+  // These methods are used by both projects
+  Filter: function(filters) {
+    if (!filters) {
+      return '';
+    }
+    return `.filter(${filters})`;
+  },
+
+  Sort: function(attr) {
+    if (!attr) {
+      return '';
+    }
+    return `.sort(${attr})`;
+  },
+
+  contains: function(attr, value) {
+    return attr.map(function(attr) {
+      return `contains(${attr}, '${value}')`;
+    }).join(' | ');
+  },
+
+};
+
+module.exports = HTSQL;
