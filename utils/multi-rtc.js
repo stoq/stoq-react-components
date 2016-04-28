1eacafa38e6d87ee475d6fe0e42c70bee022659c
Eduardo Nishizuka <nishizuka23@gmail.com>
2016-04-28 12:03:16 -0300

    Change webRTC communication when sending big amount of data.
    -Send chunks of data instead of everything
    
    Change-Id: Ib414a70be829610979eb5b6cd73d6dd9ccdff3c8

--- utils/multi-rtc.js
+++ utils/multi-rtc.js
@@ -5,6 +5,10 @@ function MultiRTC(options) {
   this.peers = {};
   this.pending = {};
   this.responses = {};
+  this.data = {};
+  this.chunks = {};
+  this.DATA_LIMIT = 6144;
+  this.CHUNK_SIZE = 4096;
 
   this.signaller = options.signaller;
   this.metadata = options.metadata;
@@ -42,6 +46,18 @@ MultiRTC.prototype.send = function(data, id, onResponse) {
   // Use a single id, if it is provided
   var ids = id ? [id] : Object.keys(this.peers);
   data = JSON.stringify(data);
+
+  // If the data is more than the limit (6144 bytes), send chunks
+  // of data.
+  if (data.length > this.DATA_LIMIT) {
+    var dataId = Math.random().toString(32).split('.')[1];
+    this.data[dataId] = data;
+    data = JSON.stringify({
+      __id__: dataId,
+      __length__: data.length,
+      chunk: data.slice(0, this.CHUNK_SIZE),
+    });
+  }
 
   ids.forEach(function(key) {
     // If the peer channel is open and available, send the message right away
@@ -97,6 +113,47 @@ MultiRTC.prototype.onOpen = function(id) {
  */
 MultiRTC.prototype.onMessage = function(id, event) {
   var data = JSON.parse(event.data);
+
+  if (data.__type__ === 'destroy') {
+    delete this.data[data.__id__];
+    return;
+  }
+
+  // Send the remaining chunks
+  if (data.__offset__) {
+    var dataId = data.__id__;
+    var offset = data.__offset__;
+    return this.send({
+      __id__: dataId,
+      chunk: this.data[dataId].slice(offset, offset + this.CHUNK_SIZE),
+    }, id);
+  }
+
+  // Receive a chunk of data
+  if (data.__id__) {
+    var chunk = this.chunks[data.__id__] || {
+      length: data.__length__,
+      content: '',
+    };
+    this.chunks[data.__id__] = chunk;
+    chunk.content += data.chunk;
+
+    // Requesting more chunks of data
+    if (chunk.content.length < chunk.length) {
+      return this.send({
+        __id__: data.__id__,
+        __offset__: chunk.content.length,
+      }, id);
+    }
+
+    // Gathering the data and destroying the requests
+    data = JSON.parse(chunk.content);
+    delete this.chunks[data.__id__];
+    this.send({
+      __id__: id,
+      __type__: 'destroy',
+    });
+  }
 
   // When receiving a message that has a __response_id__ respond only to it,
   // without ever triggering the `data` callback.
