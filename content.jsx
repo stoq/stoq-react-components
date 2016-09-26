a35c16e46cf27add35b762839c6eef5ab55a9821
Eduardo Nishizuka <nishizuka23@gmail.com>
2016-09-26 14:11:44 -0300

    Show explicitly which date is being filtered.
    Change-Id: Ic66f96322c38d5fb5cdbd3bf335db37fb5ac2a97

--- content.jsx
+++ content.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
-
 import Utils from 'utils';
+import Moment from 'moment';
 
 /* AdminLTE Content to be displayed
  *
@@ -37,6 +37,25 @@ module.exports.Header = class Header extends React.Component {
     let title = $('title').html().split('-')[0];
     let pageTitle = Utils.getValue(this.props.title);
     let pageSubtitle = Utils.getValue(this.props.subtitle);
+    let filterLabel = '';
+    if (this.props.hasDateFilter) {
+      let dateParts = this.props.filterDate.split('to');
+      if (dateParts.length == 2) {
+        // Advanced date filter
+        filterLabel = ` - filtrado no período ${Moment(dateParts[0]).format('DD/MM/YYYY')} a
+                        ${Moment(dateParts[1]).format('DD/MM/YYYY')}`;
+      } else {
+        let day = Moment(this.props.filterDate).format('D');
+        let month = Moment(this.props.filterDate).format('MMMM');
+        let year = Moment(this.props.filterDate).format('Y');
+        let group = {
+          day: 'dia ' + day + ' de ' + month + ' de ' + year,
+          month: 'mês de ' + month + ' de ' + year,
+          year: 'ano de ' + year,
+        };
+        filterLabel = ` - filtrado pelo ${group[this.props.filterGroup]}`;
+      }
+    }
     $('title').text(`${title} - ${pageTitle} ${pageSubtitle}`);
     return <section className="content-header">
       <h1 className="page-title">
@@ -44,6 +63,7 @@ module.exports.Header = class Header extends React.Component {
           { this.props.title }&nbsp;
           <small className="hidden-xs">
             { this.props.subtitle }
+            { filterLabel }
           </small>
         </div>
         { this.props.children }
