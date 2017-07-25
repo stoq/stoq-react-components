cd8ccccc4923f959cec62648eb6ca237e0692b4c
Eduardo Nishizuka <nishizuka23@gmail.com>
2017-07-25 11:12:53 -0300

    Add className prop to LoadingButton
    Change-Id: I5ab9b8f0ec710a2f8a8e48064117f40107e8bcd5

--- loading-button.jsx
+++ loading-button.jsx
@@ -4,7 +4,7 @@ import Utils from './utils';
 
 var LoadingButton = React.createClass({
   render: function() {
-    return <button {...Utils.only(this.props, ['onClick', 'style', 'title', 'type'])} disabled={this.props.loading || this.props.disabled}>
+    return <button {...Utils.only(this.props, ['onClick', 'style', 'title', 'type', 'className'])} disabled={this.props.loading || this.props.disabled}>
       {this.props.loading ? <i className="fa fa-refresh fa-spin"/>  : this.props.children}
     </button>;
   },
